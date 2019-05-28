a5149368e1f63074700937ec27269e6ad4d68429
PartMan7 <47669599+PartMan7@users.noreply.github.com>
2019-05-28 11:14:51 +0530

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,82 @@
+var SDClient = require('./client.js');
+var config = require('./config.js');
+
+var options = {
+	serverid: config.serverid,
+	loginServer: 'https://play.pokemonshowdown.com/~~' + config.serverid +'/action.php',
+	nickName: config.nickName,
+	pass: config.pass,
+	autoJoin: config.autoJoin
+};
+
+var Bot = new SDClient(config.server, config.port, options);
+
+function toId(text) {
+	return text.toLowerCase().replace(/[^a-z0-9]+/g, '');
+}
+
+Bot.connect();
+
+Bot.on('connect', function (connection) {
+	console.log('Connection established.');
+});
+Bot.on('chat', function (room, time, by, message) {
+
+	var admin = toId(config.admin);
+    var prefix = config.prefix;
+    if (!message.startsWith(prefix)) return;
+    messagecontent = message.substr((prefix).length);
+    const args = messagecontent.split(' ');
+    const command = args.shift().toLowerCase();
+
+    if (toId(by) == admin || by == admin) {
+    	//Admin commands
+
+    	if (command === 'do') {
+    		Bot.say(room, args.join(' '));
+    	}
+
+    	if (command === 'eval') {
+    		try {
+    		    Bot.say(room, eval(args.join(' ')));
+    		}
+    		catch (e) {
+    			Bot.say(room, e.message);
+    			console.log(e.stack);
+    		}
+    	}
+
+    	if (command === 'kill') {
+    		Bot.disconnect();
+    	}
+    }
+
+    if (toId(by) == admin || by.startsWith('@') || by.startsWith('#') || by.startsWith('&') || by.startsWith('~') || by == admin) {
+    	// @ commands
+
+    	if (command === 'echo') {
+    		Bot.say(room, ` ${args.join(' ')}`);
+    	}
+
+    }
+
+    if (toId(by) == admin || by.startsWith('%') || by.startsWith('@') || by.startsWith('#') || by.startsWith('&') || by.startsWith('~') || by == admin) {
+    	// % commands
+    }
+
+    if (toId(by) == admin || by.startsWith('+') || by.startsWith('%') || by.startsWith('@') || by.startsWith('#') || by.startsWith('&') || by.startsWith('~') || by == admin) {
+    	//+ commands
+    }
+
+    if (1 === 1) {
+    	//Unrestricted commands
+
+    	if (command === 'help') {
+    		Bot.say(room, '-_-');
+    	}
+
+    	if (command === 'ping') {
+    		Bot.say(room, 'Pong!');
+    	}
+    }
+});
---
+++ config.js
@@ -0,0 +1,16 @@
+exports.admin = 'ADMIN';
+// User who can use all commands of the bot
+exports.server = 'sim.smogon.com';
+// sim.smogon.com, is the server for Pokemon Showdown
+exports.port = 8000;
+// Port 8000 is the default
+exports.serverid = 'showdown';
+// The id of the server you wish to join, 'showdown' is the default
+exports.nickName = 'BOTNAME';
+// Username of the Pokemon Showdown account you wish the bot to login into
+exports.pass = 'PASSWORD';
+// Password of the Pokemon Showdown account you wish the bot to login into, if the account is not registered then leave blank
+exports.autoJoin = ['botdev'];
+// Rooms the bot will join as soon as it connects to Pokemon Showdown
+exports.prefix = 'PREFIX';
+// The character(s) used before a command
